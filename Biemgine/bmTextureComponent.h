d5849070976b462cb5668588b25141507e25ef04
Jonathan <jgimmink@avans.nl>
2017-10-18 13:34:51 +0200

    Added layer in texture component

--- Biemgine/bmTextureComponent.h
+++ Biemgine/bmTextureComponent.h
@@ -4,7 +4,8 @@ class bmTextureComponent :
     public bmComponent
 {
 public:
-    bmTextureComponent(std::string path, float offsetX, float offsetY, int w = -1, int h = -1) : path(path), offsetX(offsetX), offsetY(offsetY), width(w), height(h) { };
+    bmTextureComponent(std::string path, float offsetX, float offsetY, int w = -1, int h = -1, unsigned int layer = 0)
+        : path(path), offsetX(offsetX), offsetY(offsetY), width(w), height(h), layer(layer) { };
 
     std::string& getPath() { return path; };
 
@@ -24,9 +25,14 @@ public:
         return offsetY;
     }
 
+    unsigned int getLayer() const {
+        return layer;
+    }
+
 private:
     std::string path;
     int width, height;
     float offsetX, offsetY;
+    unsigned int layer;
 };
 
