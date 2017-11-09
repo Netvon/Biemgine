d7223434b8aecc9909e108920a7e1cb9a0efdd32
Kevin <fruitwasp@users.noreply.github.com>
2017-11-09 10:36:27 +0100

    spacebiem namespace

--- SpaceBiem/systems/bmGravitySystem.h
+++ SpaceBiem/systems/bmGravitySystem.h
@@ -6,19 +6,21 @@
 using namespace std;
 using namespace biemgine;
 
-class bmGravitySystem :
-    public bmSystem
+namespace spacebiem
 {
-public:
-    void update(const bmEntity& entity) override;
-    void after() override;
+    class bmGravitySystem :
+        public bmSystem
+    {
+    public:
+        void update(const bmEntity& entity) override;
+        void after() override;
 
-    void applyForce(bmVector centerOfGravity, bmVector centerOfSatellite, bmPhysicsComponent * satPhysics, bmAffectedByGravityComponent* affected);
+        void applyForce(bmVector centerOfGravity, bmVector centerOfSatellite, bmPhysicsComponent * satPhysics, bmAffectedByGravityComponent* affected);
 
-    void onSceneSwitch() override {};
-    
-private:
-    vector<const bmEntity*> gravityPoints;
-    vector<const bmEntity*> satellites;
-};
+        void onSceneSwitch() override {};
 
+    private:
+        vector<const bmEntity*> gravityPoints;
+        vector<const bmEntity*> satellites;
+    };
+}
