fdc9a5a3feb72c9d2a20b6ee721b3ca15948fa98
stijnie2210 <stijnmommersteeg@gmail.com>
2017-11-10 13:04:24 +0100

    Scores change color based on name

--- SpaceBiem/systems/ScoreUISystem.cpp
+++ SpaceBiem/systems/ScoreUISystem.cpp
@@ -1,9 +1,12 @@
 #include "stdafx.h"
+#include <windows.h>
+#include <Lmcons.h>
 #include "ScoreUISystem.h"
 
 using biemgine::TextComponent;
 using biemgine::PositionComponent;
 using biemgine::UIComponent;
+using biemgine::Color;
 
 namespace spacebiem
 {
@@ -49,8 +52,24 @@ namespace spacebiem
             else oRef = oc;
         }
 
+        TCHAR user[UNLEN + 1];
+        DWORD size = UNLEN + 1;
+        string name = "Mr. NoName";
+
+        if (GetUserName((TCHAR*)user, &size)) {
+            wstring test(&user[0]);
+            string stringName(test.begin(), test.end());
+            name = stringName;
+        }
+
         auto tx = entity.getComponent<TextComponent*>("text");
-        tx->setText(oRef->getName() + ": " + std::to_string((int)oRef->getScore()));
+
+        if (name == oRef->getName()) {
+            tx->setText(oRef->getName() + ": " + std::to_string((int)oRef->getScore()), { 66, 113, 244 });
+        }
+        else {
+            tx->setText(oRef->getName() + ": " + std::to_string((int)oRef->getScore()), { 255, 255, 255 });
+        }
     }
 
     void ScoreUISystem::after(const float deltaTime) {}
