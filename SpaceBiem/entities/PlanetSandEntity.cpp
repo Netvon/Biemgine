9c3d4c846a089cef705128cdfe38bd2d6fb8615e
luukspierings <lcpspier@avans.nl>
2017-11-10 10:56:44 +0100

    Updated textures

--- SpaceBiem/entities/PlanetSandEntity.cpp
+++ SpaceBiem/entities/PlanetSandEntity.cpp
@@ -16,9 +16,9 @@ namespace spacebiem
     PlanetSandEntity::PlanetSandEntity(float x, float y, Color color, float w, float h, float atmosphere)
     {
         addComponent("position", new PositionComponent(x, y));
-        addComponent("texture", new TextureComponent("textures/earth-plain.png", 0.f, 0.f, w, h, 2u));
+        addComponent("texture", new TextureComponent("textures/sand-plain.png", 0.f, 0.f, w, h, 2u));
         //addComponent("texture", new TextureComponent("textures/atmosphere_clouds.png", 0.f - ((w * 2.5f / 2) - w / 2), 0 - ((h * 2.5f / 2) - h / 2), w * 2.5f, h * 2.5f, 9999u));
-        addComponent("texture", new TextureComponent("textures/earth-TypeA.png", 0 - ((w*1.19f / 2) - w / 2), 0 - ((h*1.19f / 2) - h / 2), w*1.19f, h*1.19f, 1u));
+        addComponent("texture", new TextureComponent("textures/sand-TypeA.png", 0 - ((w*1.22f / 2) - w / 2), 0 - ((h*1.22f / 2) - h / 2), w*1.22f, h*1.22f, 1u));
         addComponent("texture", new TextureComponent("textures/atmosphere.png", w / -2.f, h / -2.f, w * 2.f, h * 2.f));
         addComponent("texture", new TextureComponent("textures/gravityField.png", w / -2.f, h / -2.f, w * 2.f, h * 2.f));
         addComponent("physics", new PhysicsComponent(w, h, true, PhysicsComponentShape::CIRCLE));
