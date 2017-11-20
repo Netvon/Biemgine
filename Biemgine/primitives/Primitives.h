dfaa1027a6b6784abc0b1e4e11a9be60723b91b8
ViezeFredsel <wngeck@avans.nl>
2017-11-20 15:14:32 +0100

    oeps

--- Biemgine/primitives/Primitives.h
+++ Biemgine/primitives/Primitives.h
@@ -22,7 +22,17 @@ namespace biemgine
         Color& WithGreen(unsigned char green);
         Color& WithBlue(unsigned char green);
 
+        static Color EarthAtmosphere(){
+            return Color{ 71, 166, 245, 255 };
+        }
 
+        static Color SandAtmosphere() {
+            return Color{ 213, 207, 105, 255 };
+        }
+
+        static Color ToxicAtmosphere() {
+            return Color{ 20, 221, 53, 255 };
+        }
 
     };
 
