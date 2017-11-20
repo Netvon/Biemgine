45a3766125dc5abf833fbb22aaa5a6a402cd84ed
stijnie2210 <stijnmommersteeg@gmail.com>
2017-11-20 13:18:11 +0100

    TG-197 planetsprites for score per planet

--- SpaceBiem/scenes/GameoverScene.cpp
+++ SpaceBiem/scenes/GameoverScene.cpp
@@ -5,6 +5,8 @@
 
 #include "..\entities\PlanetEarthEntity.h"
 #include "..\entities\PlanetMoonEntity.h"
+#include "..\entities\PlanetSandEntity.h"
+#include "..\entities\PlanetToxicEntity.h"
 #include "..\entities\ButtonUIEntity.h"
 #include "..\factories\ScoreUIFactory.h"
 #include "..\entities\ScoreUIEntity.h"
@@ -39,32 +41,44 @@ namespace spacebiem
         float resourcesX = x - 150.f;
         float planetsX = x + 150.f;
 
-        planetsScore["sand"] = 10;
-        planetsScore["earth"] = 13;
-        planetsScore["toxic"] = 43;
-        planetsScore["moon"] = 65;
+        /*planetsScore["sand"] = 3;
+        planetsScore["earth"] = 5;
+        planetsScore["toxic"] = 7;
+        planetsScore["moon"] = 1;*/
 
         addEntity<SpriteEntity>("textures/game_over.png", static_cast<float>(x - 50.f), 100.f, Color::White(), -1, -1, 100u);
-        //addEntity<TextUIEntity>(static_cast<float>(x), 150.f, Color{ 66, 143, 244 }, "Je score was: " + std::to_string(score));
         addEntity<TextUIEntity>(resourcesX, 200.f, Color{ 66, 143, 244 }, "Resources");
         addEntity<TextUIEntity>(planetsX, 200.f, Color{ 66, 143, 244 }, "Planets");
 
         float heightCounter = 250;
 
         for (auto& r : resources) {
-            addEntity<SpriteEntity>("textures/" + r.first + ".png", resourcesX, heightCounter, Color::White(), 20, 20, 100u);
-            addEntity<TextUIEntity>(resourcesX + 25, heightCounter, Color{ 66, 143, 244 }, " x " + std::to_string(r.second));
+            addEntity<SpriteEntity>("textures/" + r.first + ".png", resourcesX, heightCounter, Color::White(), 40, 40, 100u);
+            addEntity<TextUIEntity>(resourcesX + 45, heightCounter + 20, Color{ 66, 143, 244 }, " x " + std::to_string(r.second));
             heightCounter = heightCounter + 50.f;
         }
 
         heightCounter = 250;
 
         for (auto& p : planetsScore) {
-            addEntity<TextUIEntity>(planetsX, heightCounter, Color{ 66, 143, 244 }, p.first + ": " + std::to_string(p.second));
-            heightCounter = heightCounter + 50.f;
+            if (p.first == "earth") {
+                addEntity<PlanetEarthEntity>(planetsX, heightCounter, Color{ 71, 166, 245, 255 }, 40, 40, 0, 10.f);
+            }
+            else if (p.first == "moon") {
+                addEntity<PlanetMoonEntity>(planetsX, heightCounter, Color::White(), 40, 40, 0);
+            }
+            else if (p.first == "sand") {
+                addEntity<PlanetSandEntity>(planetsX, heightCounter, Color{ 213,207,105,255 }, 40, 40, 0, 10.f);
+            }
+            else if (p.first == "toxic") {
+                addEntity<PlanetToxicEntity>(planetsX, heightCounter, Color{ 127, 0, 255, 255 }, 40, 40, 0, 10.f);
+            }
+            
+            addEntity<TextUIEntity>(planetsX + 60, heightCounter + 15, Color{ 66, 143, 244 }, " x " + std::to_string(p.second));
+            heightCounter = heightCounter + 85.f;
         }
 
-        addEntity<PlanetEarthEntity>(-100.f, static_cast<float>(wH - 200), Color::White(), planetWidth, planetHeight, 0, 10.f);
+        addEntity<PlanetEarthEntity>(-100.f, static_cast<float>(wH - 200), Color{ 71, 166, 245, 255 }, planetWidth, planetHeight, 0, 10.f);
         addEntity<PlanetMoonEntity>(static_cast<float>(wW - 250), static_cast<float>(wH - 250), Color::White(), planetWidth, planetHeight, 0);
 
         auto buttonTexture = "textures/button_white.png";
