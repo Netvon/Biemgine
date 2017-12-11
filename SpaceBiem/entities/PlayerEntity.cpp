bbba816fa0b7aa1c0697ed65df1619de250b0876
Tom van Nimwegen <tomvannimwegen@gmail.com>
2017-12-11 21:15:40 +0100

    Updated player sprite

--- SpaceBiem/entities/PlayerEntity.cpp
+++ SpaceBiem/entities/PlayerEntity.cpp
@@ -25,7 +25,7 @@ namespace spacebiem
     {
         addComponent("position", new PositionComponent(x, y));
         addComponent("color", new ColorComponent(color));
-        addComponent("texture", new AnimatedTextureComponent("textures/PlayerSpriteSheet.png", 0, 0, TextureColumnDef{ 9llu, 128 }, TextureRowDef{ 1llu, 256 }, 7.5f / 2.0f, w, h, 5u, true, "background"));
+        addComponent("texture", new AnimatedTextureComponent("textures/PlayerSpriteSheet2.png", 0, 0, TextureColumnDef{ 8llu, 128 }, TextureRowDef{ 1llu, 256 }, 7.5f / 2.0f, w, h, 5u, true, "background"));
         addComponent("physics", new PhysicsComponent(w, h, false, PhysicsComponentShape::RECTANGLE, mass));
         addComponent("oxygen", new OxygenComponent);
         addComponent("grounded", new GroundedComponent);
