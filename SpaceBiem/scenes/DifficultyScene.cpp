5c791a6812f3ad6e812e58122d5f3cc496ad5ec9
luukspierings <lcpspier@avans.nl>
2017-12-11 13:52:41 +0100

    Added difficulty texture at choosing difficulty

--- SpaceBiem/scenes/DifficultyScene.cpp
+++ SpaceBiem/scenes/DifficultyScene.cpp
@@ -33,8 +33,7 @@ namespace spacebiem
         int w = 50;
         int x = wW / 2 - w;
 
-        addEntity<SpriteEntity>("textures/spacebiem.png", wW / 2 - 175, 100, Color::White(), -1, -1);
-        addEntity<SpriteEntity>("textures/player-standing.png", wW / 2 - 175 + 260, 115, Color::White(), playerWidth, playerHeight);
+        addEntity<SpriteEntity>("textures/choose_difficulty.png", wW / 2 - 175, 130, Color::White(), -1, -1);
         addEntity<PlanetEarthEntity>(-100.f, static_cast<float>(wH - 200), Color{ 71, 166, 245, 255 }, planetWidth, planetHeight, 0, 10.f);
         addEntity<PlanetMoonEntity>(static_cast<float>(wW - 250), static_cast<float>(wH - 250), Color::White(), planetWidth, planetHeight, 0);
 
@@ -43,7 +42,7 @@ namespace spacebiem
         auto buttonTextColor = Color::White();
         auto buttonSize = Size{ 150, 50 };
 
-        int beginY = 380;
+        int beginY = 280;
         int incr = 65;
 
         addEntity<ButtonUIEntity>(x - 25, beginY + (0 * incr), Color{ 22, 94, 22 }, buttonTextColor, buttonSize, "Normal", buttonTexture,
@@ -64,7 +63,7 @@ namespace spacebiem
             manager->navigateTo<LevelScene>(true, Difficulty::EXPERT);
         });
 
-        beginY += 20;
+        beginY += 40;
         addEntity<ButtonUIEntity>(x - 25, beginY + (3 * incr), buttonColor, buttonTextColor, buttonSize, "Menu", buttonTexture,
             [this](StateManager* manager)
         {
