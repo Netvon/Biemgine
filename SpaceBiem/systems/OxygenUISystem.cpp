59021f3b810695550bc47eca4dcda34684da2b98
Jonathan <jgimmink@avans.nl>
2017-12-15 22:56:30 +0100

    Changed Oxygen ui system

--- SpaceBiem/systems/OxygenUISystem.cpp
+++ SpaceBiem/systems/OxygenUISystem.cpp
@@ -1,72 +1,57 @@
 #include "stdafx.h"
 #include "OxygenUISystem.h"
 
-using biemgine::Color;
-using biemgine::ColorComponent;
-using biemgine::UIComponent;
-using biemgine::PositionComponent;
-using biemgine::RectangleComponent;
-using biemgine::TextureComponent;
-
 namespace spacebiem
 {
-    void OxygenUISystem::update(const Entity & entity)
+    void OxygenUISystem::onAddEntity(Entity & entity)
     {
         if (!entity.hasComponent("oxygen")) return;
 
-        auto oc = entity.getComponent<OxygenComponent>("oxygen");
-
-        if (!entity.hasComponent("ui")) {
-            if (oxygenMap.find(oc) == oxygenMap.end()) {
-                oxygenMap[oc] = false;
-            }
-            return;
-        }
-
-        if (!entity.hasComponent("position") || !entity.hasComponent("texture"))
-            return;
-
-        auto pc = entity.getComponent<PositionComponent>("position");
-        auto uc = entity.getComponent<UIComponent>("ui");
-
-        auto tc = entity.getComponents<TextureComponent>("texture");
-        std::shared_ptr<TextureComponent> texture = nullptr;
-
-       
-
-        // If the UI doesn't have the component which to draw, pick one from the map.
-        auto oRef = uc->getComponentReference<OxygenComponent>();
-        if (oRef == nullptr) {
-            for (auto& x : oxygenMap)
-            {
-                if (!x.second) {
-                    uc->setComponentReference(x.first);
-                    oRef = x.first;
-                    break;
-                }
-            }
-            if (oRef != nullptr) oxygenMap[oRef] = true;
-            else return;
-        }
-
-        float oBar = oRef->getOxygenAmount() / static_cast<float>(oRef->getOxygenMax());
-        float vignet = oRef->getOxygenAmount() / (static_cast<float>(oRef->getOxygenMax() / 2));
-        float alphaVignet = 255 * (1 - vignet);
-
-        for (auto& tex : tc)
+        if (!entity.hasComponent("ui"))
         {
-            if (tex->getTag() == "oxygenbar") texture = tex;
-            if (tex->getTag() == "vignet" && vignet <= 1) tex.get()->setColor(tex.get()->getColor().WithAlpha(alphaVignet));
+            OxygenEntry oxygenEntry;
+            oxygenEntry.entity = &entity;
+            oxygenEntry.oxygenComponent = entity.getComponent<OxygenComponent>("oxygen");
+
+            oxygenEntries.push_back(std::move(oxygenEntry));
         }
 
-        Color dangerRedColor { 204, 94, 94 };
-        Color newColor{
-            dangerRedColor.r + static_cast<char>((texture->getOriginalColor().r - dangerRedColor.r) * oBar),
-            dangerRedColor.g + static_cast<char>((texture->getOriginalColor().g - dangerRedColor.g) * oBar),
-            dangerRedColor.b + static_cast<char>((texture->getOriginalColor().b - dangerRedColor.b) * oBar)
-        };
 
-        texture->setWidth(static_cast<int>(texture->getOriginalWidth() * oBar));
-        texture->setColor(newColor);
+        TextureEntry textureEntry;
+        textureEntry.entity = &entity;
+        textureEntry.textureComponents = entity.getComponents<TextureComponent>("texture");
+
+        textureEntries.push_back(std::move(textureEntry));
+    }
+
+    void OxygenUISystem::update()
+    {
+        for (OxygenEntry entry : oxygenEntries)
+        {
+            float oBar = entry.oxygenComponent->getOxygenAmount() / static_cast<float>(entry.oxygenComponent->getOxygenMax());
+            float vignet = entry.oxygenComponent->getOxygenAmount() / (static_cast<float>(entry.oxygenComponent->getOxygenMax() / 2));
+            float alphaVignet = 255 * (1 - vignet);
+
+            for (TextureEntry textureEntry : textureEntries)
+            {
+                for (auto tex : textureEntry.textureComponents)
+                {
+                    if (tex->getTag() == "oxygenbar")
+                    {
+                        Color dangerRedColor{204, 94, 94};
+                        Color newColor{
+                            dangerRedColor.r + static_cast<char>((tex->getOriginalColor().r - dangerRedColor.r) * oBar),
+                            dangerRedColor.g + static_cast<char>((tex->getOriginalColor().g - dangerRedColor.g) * oBar),
+                            dangerRedColor.b + static_cast<char>((tex->getOriginalColor().b - dangerRedColor.b) * oBar)
+                        };
+
+                        tex->setWidth(static_cast<int>(tex->getOriginalWidth() * oBar));
+                        tex->setColor(newColor);
+                    }
+
+                    if (tex->getTag() == "vignet" && vignet <= 1) tex.get()->setColor(tex.get()->getColor().WithAlpha(alphaVignet));
+                }  
+            }
+        }
     }
 }
