30a33c0fa119b41459786c8ffc202be163519ac4
Jonathan <jgimmink@avans.nl>
2017-12-16 13:16:24 +0100

    Changed score ui system

--- SpaceBiem/systems/ScoreUISystem.h
+++ SpaceBiem/systems/ScoreUISystem.h
@@ -6,17 +6,38 @@
 using biemgine::GraphicsDevice;
 using biemgine::System;
 using biemgine::Entity;
+using biemgine::TextComponent;
+using biemgine::PositionComponent;
+using biemgine::UIComponent;
+using biemgine::Color;
 
 namespace spacebiem
 {
     class ScoreUISystem : public System
     {
     public:
+        struct PlayerEntry
+        {
+            Entity* entity;
+            std::shared_ptr<ScoreComponent> scoreComponent;
+        };
+
+        struct ScoreUIEntry
+        {
+            Entity* entity;
+            std::shared_ptr<ScoreComponent> scoreComponent;
+            std::shared_ptr<TextComponent> textComponent;
+        };
+
         ~ScoreUISystem() {};
 
+        void onAddEntity(Entity& entity) override;
+        void update() override;
         void update(const Entity& entity) override;
 
     private:
+        vector<PlayerEntry> playerEntries;
+        vector<ScoreUIEntry> scoreUIEntries;
         std::map<std::shared_ptr<ScoreComponent>, bool> scoreMap;
     };
 }
