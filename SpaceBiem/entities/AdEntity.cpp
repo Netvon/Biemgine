ce85ec05d7e6cb8b7f21d358c952c1f1f5604bee
Jonathan <jgimmink@avans.nl>
2017-12-23 00:01:52 +0100

    Added billboard texture

--- SpaceBiem/entities/AdEntity.cpp
+++ SpaceBiem/entities/AdEntity.cpp
@@ -4,15 +4,16 @@ namespace spacebiem
 {
     AdEntity::AdEntity()
     {
-        addComponent("position", new PositionComponent(1493, -169));
-        addComponent("text", new TextComponent(Fonts::Roboto(), "X", Color{ 0, 0, 0 }, true, 25, 25, 1100u, true));
-        addComponent("texture", new TextureComponent(Ads::instance().random(), 0, 0, 402, 169, 1000u, true, "", Color::White()));
-
+        addComponent("position", new PositionComponent(1493, -191));
+        addComponent("text", new TextComponent(Fonts::Roboto(), "X", Color{ 0, 0, 0 }, true, 20, 169, 1100u, true));
+        addComponent("texture", new TextureComponent(Ads::instance().random(), 4, 50, 382, 134, 1000u, true, "", Color::White()));
+        addComponent("texture", new TextureComponent("textures/billboard.png", 0, 0, 391, 191, 1001u, true, "", Color::White()));
+       
         auto position = getComponent<PositionComponent>("position");
-        addComponent("animation", new AnimationComponent(-169, 25, 500, [position](float newValue) { position->setY(newValue); }, nullptr, true));
+        addComponent("animation", new AnimationComponent(-191, 0, 500, [position](float newValue) { position->setY(newValue); }, nullptr, true));
 
         auto animation = getComponent<AnimationComponent>("animation");
-        addComponent("ui", new UIComponent(SizeRect{Point{0,0,0}, Size{50, 50}}, [animation](StateManager* e) { animation->playReversed(); }, nullptr));
+        addComponent("ui", new UIComponent(SizeRect{Point{0,159,0}, Size{20, 20}}, [animation](StateManager* e){ animation->playReversed(); }, nullptr));
     }
 }
 
