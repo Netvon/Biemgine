c57ea38b43cab0f2609a785e1ab0a2653655f293
ViezeFredsel <wngeck@avans.nl>
2018-01-15 11:44:20 +0100

    entity

--- SpaceBiem/entities/WormholeEntity.cpp
+++ SpaceBiem/entities/WormholeEntity.cpp
@@ -1,12 +1,24 @@
-#include "stdafx.h"
+#include "Biemgine.h"
+
 #include "WormholeEntity.h"
+#include "../components/AtmosphereComponent.h"
+#include "../components/GravityComponent.h"
 
+using biemgine::AudioComponent;
+using biemgine::PositionComponent;
+using biemgine::TextureComponent;
+using biemgine::PhysicsComponent;
+using biemgine::ColorComponent;
+using biemgine::GroundComponent;
+using biemgine::PhysicsComponentShape;
 
-WormholeEntity::WormholeEntity()
+namespace spacebiem
 {
-}
-
-
-WormholeEntity::~WormholeEntity()
-{
+    WormholeEntity::WormholeEntity(float x, float y, Color color, float w, float h, int pScoreBonus, float atmosphere, const string& pName, bool scoreGiven) :
+        PlanetEntity(x, y, color, w, h, "textures/wormhole-plain.png", "textures/wormhole-TypeA.png", pScoreBonus, pName, scoreGiven)
+    {
+        setTag("wormhole");
+        addComponent("texture", new TextureComponent("textures/wormhole-TypeB.png", 0.f, 0.f, w, h, 2u, true, "background_B"));
+        //addComponent("audio", new AudioComponent("audio/earth.mp3", -1, -1, 48, 1000));
+    }
 }
