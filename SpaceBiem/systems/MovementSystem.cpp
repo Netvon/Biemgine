c4efc02860861a93d206b14e6e7d769400855232
Kevin <fruitwasp@users.noreply.github.com>
2018-01-16 11:41:31 +0100

    less bounceback force on collision with AI

--- SpaceBiem/systems/MovementSystem.cpp
+++ SpaceBiem/systems/MovementSystem.cpp
@@ -72,8 +72,8 @@ namespace spacebiem
                     auto force = -entry.physicsComponent->getForceX() * 1.1;
                     auto forceY = -entry.physicsComponent->getForceY() * 1.1;
 
-                    force = max<int>(force, 100);
-                    forceY = max<int>(forceY, 100);
+                    force = max<int>(force, 20);
+                    forceY = max<int>(forceY, 20);
 
                     entry.physicsComponent->addImpulse("bounceback", force, forceY);
                 }
